8a1bb2ff6ec2c34e694a12f6abc34e6874fd4457
Carlos <cmardonespino@gmail.com>
2025-04-13 18:49:12 -0400

    se agrega profile para instancias

---
+++ infra/terraform/modules/aws_eks/profile.tf
@@ -0,0 +1,4 @@
+resource "aws_iam_instance_profile" "profile" {
+  name     = "eks-${var.eks_cluster_name}-${var.eks_nodegroup_name}"
+  role     = aws_iam_role.nodegroup.name # role associated to the ec2 instances
+}
