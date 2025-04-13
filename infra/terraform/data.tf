f0861b825c09edddcf874433f744a32ed7a22ff0
Carlos <cmardonespino@gmail.com>
2025-04-13 18:53:40 -0400

    se agrega data source para obtener vpc y subnets publicas

--- infra/terraform/data.tf
+++ infra/terraform/data.tf
@@ -1,6 +1,20 @@
+data "aws_vpc" "devexhiringtest" {
+  filter {
+    name   = "tag:Name"
+    values = ["ws-test-vpc"]
+  }
+}
+
 data "aws_subnets" "private" {
   filter {
     name   = "tag:Name"
     values = ["ws-test-subnet-private*-us-east-1*"]
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "tag:Name"
+    values = ["ws-test-subnet-public*-us-east-1*"]
   }
 }
