33d8d1191569b255be8f36d42aa33a5de7901167
Carlos <cmardonespino@gmail.com>
2025-04-13 18:54:01 -0400

    se agregan variables como input en base a lo solicitado por el modulo de eks

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -1,5 +1,9 @@
 module "eks_devexhiringtest_cluster" {
   source                 = "./modules/aws_eks"
+  vpc_id                 = data.aws_vpc.devexhiringtest.id
   eks_cluster_name       = "devexhiringtest-cmardonesp"
   eks_cluster_subnet_ids = data.aws_subnets.private.ids
+
+  eks_nodegroup_name       = "ng-devexhiringtest"
+  eks_nodegroup_subnet_ids = data.aws_subnets.public.ids
 }
