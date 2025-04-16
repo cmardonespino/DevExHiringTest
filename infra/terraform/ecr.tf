21812970ea95fb328b5b42afc8c1bdd3937088f5
Carlos <cmardonespino@gmail.com>
2025-04-16 01:59:56 -0400

    se agrega lifecycle para ecr

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -6,3 +6,38 @@ resource "aws_ecr_repository" "devexhiringtest" {
     scan_on_push = false
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "devexhiringtest" {
+  repository = aws_ecr_repository.devexhiringtest.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Eliminar imagenes sin tag"
+        selection = {
+          tagStatus     = "untagged"
+          countType     = "sinceImagePushed"
+          countUnit     = "days"
+          countNumber   = 1
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Mantener solo las ultimas 5 imagenes con tag"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["build-", "test-"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
